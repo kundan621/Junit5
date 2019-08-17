fc9f9d9ec3c3a6620a11631cd0de85a51843f7d7
kundan621 <kundan621@gmail.com>
2019-08-17 08:54:59 +0530

    junit commit

--- src/main/java/io/javabrains/MathUtils.java
+++ src/main/java/io/javabrains/MathUtils.java
@@ -5,4 +5,12 @@ public class MathUtils {
     public int add(int a,int b){
         return a+b;
     }
+
+    public double computeCircleArea(double radius){
+        return 3.14*radius*radius;
+    }
+
+    public int divide(int a, int b){
+        return a/b;
+    }
 }
--- src/test/java/io/javabrains/MathUtilsTest.java
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -1,5 +1,6 @@
 package io.javabrains;
 
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,8 +8,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class MathUtilsTest {
 
     @Test
-    void test(){
-        System.out.println("This test ran");
+    void testAdd(){
+        MathUtils mathutils=new MathUtils();
+        int expected=2;
+        int actual=mathutils.add(1,1);
+        assertEquals(expected,actual);
     }
 
+
+    @Test
+    void testComputeCircleRadius(){
+        MathUtils mathutils=new MathUtils();
+        assertEquals(314,mathutils.computeCircleArea(10),"should return c");
+    }
+
+
+    @Test
+    void testDivide(){
+        MathUtils mathutils=new MathUtils();
+        assertEquals(314,mathutils.divide(10,0));
+    }
 }
